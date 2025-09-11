4ca76323b496bfa49f1e0e96602a6c929bf4b47d
StudioITDA <itdda.fiberart@gmail.com>
2025-09-12 00:14:34 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PWA cache v12
-const CACHE_NAME = 'weavecalc-v12';
+const CACHE_NAME = 'weavecalc-v15';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE_NAME)?null:caches.delete(k))))); });
