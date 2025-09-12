0b962e0292d65a8cca10032171f81bf122469827
StudioITDA <itdda.fiberart@gmail.com>
2025-09-12 13:43:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PWA cache v14 (core-only, no icons)
-const CACHE_NAME = 'weavecalc-v14';
+const CACHE_NAME = 'warpcalc-v16';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
